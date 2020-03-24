3e1bd916782698e5accc25aa7ec895e5498f6c00
Visal Rajapakse <visalrajapakse@gmail.com>
2020-03-25 01:18:39 +0530

    completed implementation

--- src/max_flow/Main.java
+++ src/max_flow/Main.java
@@ -6,25 +6,26 @@ package max_flow;
 public class Main {
 
     public static void main(String[] args) {
-        executeGraph(Dataset.adjacencyMatrix96x96());
-//        executeGraph(GenerateGraph.generateDataset(6, 11));
+        executeGraph(Dataset.adjacencyMatrix12x12());
     }
 
     public static void executeGraph(int[][] graph){
         // graph implementation by adjacency matrix with a 2 dimensional array
         FordFulkersonAlgorithm ff = new FordFulkersonAlgorithm();
         int arrSize = graph.length;
-        ff.setNumberOfVertices(graph.length);    //setting the number of vertices
-        int targetNode = ff.numberOfVertices - 1;      //value of the sink node
+        ff.setNumberOfVertices(arrSize);    //setting the number of vertices
 
-        long startTime = System.nanoTime();     //start time
+        //sinkNodeValues
+        int targetNode = 5;
+        //timing the algorithm
+        long startTime = System.nanoTime();
         double maxFlow = ff.fordFulkerson(graph, targetNode);       //getting maximum-flow
         long endTime = System.nanoTime();
 
         System.out.println("The MAX FLOW of "+arrSize+"x"+arrSize+" is : " + maxFlow);
         long totalDuration = (endTime - startTime);     //calculating time taken for the algorithm execution
         System.out.println("Total duration in nano seconds : " + totalDuration);
-//        ff.printFinalGraph();
+        ff.printFinalGraph();
 
 
     }
